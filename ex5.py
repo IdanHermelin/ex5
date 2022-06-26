0744f47f2de7461e2cf39a56e85e2b203e1eef95
Idan Hermelin <idanhermelin@campus.technion.ac.il>
2022-06-26 18:36:00 +0300

    Seconf Function before debugging

--- ex5.py
+++ ex5.py
@@ -19,6 +19,7 @@ def names_of_registered_students(input_json_path, course_name):
         for student in key_list:
             if(course_name in loaded_dict[student]["registered_courses"]):
                 student_list.append(loaded_dict[student]["student_name"])
+
     return student_list
 
 def enrollment_numbers(input_json_path, output_file_path):
@@ -29,9 +30,26 @@ def enrollment_numbers(input_json_path, output_file_path):
     :param input_json_path: Path of the students database json file.
     :param output_file_path: Path of the output text file.
     """
-    pass
+    with open(input_json_path, 'r') as f:
+        course_list=[]
+        loaded_dict = json.load(f)
+        key_list = loaded_dict.keys()
+        for student in key_list:
+            course_list = list(set(input_json_path[student]["registered_courses"])| set(course_list))
 
+        course_dict = dict(zip(course_list,[0 for x in range(0,len(course_list))]))
+        #Creating the dict containing the number of students in every course
+        for student in key_list:
+            for course in input_json_path[student]["registered_courses"]:
+                course_dict[course]+=1
 
+        #Creating alphbetic list of courses
+        course_list =course_list.sort()
+
+    #Writing to the output file
+    with open(output_file_path, 'w') as f:
+        for course in course_list:
+            f.write('"' + course+ '" '+str(course_dict[course]))
 
 def courses_for_lecturers(json_directory_path, output_json_path):
     """
